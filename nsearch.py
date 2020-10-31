9c022a33d9a1b0630fec0eebaa5d8c9ded0a4909
Cristian Tangarife <cristian.tangarife@marketmix.com.co>
2020-10-31 00:25:51 -0500

    Se organiza estructura y se cambian prints para compatibilidad con python3

--- nsearch.py
+++ nsearch.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
+import os, sys
+from dbmodule import dbname, initSetup, updateApp
+from console import Console
+import re, i18n
 
-import dbmodule
-from dbmodule import *
-import console
-from console import *
-import re
-
-banner ='\033[0;36m'+'''
+banner = (
+    "\033[0;36m"
+    + """
   ================================================
     _   _  _____  _____                     _
    | \ | |/  ___||  ___|                   | |
@@ -18,21 +18,23 @@ banner ='\033[0;36m'+'''
    Version 0.4  http://goo.gl/8mFHE5  @jjtibaquira
    Email: jko@dragonjar.org  |   www.dragonjar.org
   ================================================
-'''+'\033[0m'
+"""
+    + "\033[0m"
+)
 
 # main action
-if __name__ == '__main__':
+if __name__ == "__main__":
 
-  currentLocale = re.sub('[_].*','',os.environ['LANG'])
-  i18n.load_path.append('i18n')
-  i18n.set('locale',currentLocale) if True else i18n.set('fallback','en')
+    currentLocale = re.sub("[_].*", "", os.environ["LANG"])
+    i18n.load_path.append("i18n")
+    i18n.set("locale", currentLocale) if True else i18n.set("fallback", "en")
 
-  print banner
-  if not os.path.isfile(dbmodule.dbname):
-    dbmodule.initSetup()
-  else:
-    dbmodule.updateApp()
+    print(banner)
+    if not os.path.isfile(dbname):
+        initSetup()
+    else:
+        updateApp()
 
-  os.system("clear")
-  console = console.Console()
-  console.cmdloop()
+    os.system("clear")
+    console = Console()
+    console.cmdloop()
